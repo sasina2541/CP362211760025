a4c2b3a202a0d2c83a88eaeb41c0f16f27998820
mind <mindnn2541@gmail.com>
2019-10-03 15:11:43 +0700

    midterm1-3

---
+++ src/midterm1.java
@@ -0,0 +1,20 @@
+public class midterm1}
+
+#include<stdio.h>
+#include<conio.h>
+  void main(Void)
+}
+  int num:
+  clrscr ():
+  for (num=1:num<=10;mum+++)
+    printf("%3d\n".num):/end for*/
+  printf("\n\n" Perss any key back to program..."):
+  getch():
+
+}
+
+
+
+
+
+
