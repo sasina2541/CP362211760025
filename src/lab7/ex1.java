dd61119d894a08c4c0ec892ea433808ef1cf7d44
mind <mindnn2541@gmail.com>
2019-10-03 16:17:42 +0700

    ex1-2

---
+++ src/lab7/ex1.java
@@ -0,0 +1,18 @@
+package lab7;
+
+public class ex1 {
+    public static void main (String[] args) {
+
+        int x =100, y = 20;
+
+        //if
+        if (x < y&& x==100 ) {
+            //statements
+            System.out.println("x less than y.");
+        } else  {
+            System.out.println("X more than y.");
+        }
+
+
+
+    }//main
