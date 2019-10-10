97e2342b59cf63e7ac53589b9a052616c4dfc821
mind <mindnn2541@gmail.com>
2019-10-10 14:10:07 +0700

    ex7

---
+++ src/lab7/ex8.java
@@ -0,0 +1,32 @@
+package lab7;
+
+public class ex8 {
+    public static void  main(String []args) {
+
+        for (int i = 8;i <=1 ; i++){
+            for (int j = 8; j >= i ; i++){
+            System.out.print("*");
+        }//for j
+        System.out.println();
+    }//for i
+
+    System.out.print("\n\n");
+
+    for (int i = 1; i <=8; i++) {
+        for (int j = 1; j <= i ;++){
+            System.out.print(j+" ");
+        }//for j
+        System.out.println();
+    }//for i
+    System.out.println("\n\n");
+
+  for (int i = 1;i <=8; i++){
+        for (int j = 1; j <= i ; j++){
+            System.out.print(count+" ");
+            count++;
+    }//for j
+        System.out.println();
+}//for i
+
+
+}
